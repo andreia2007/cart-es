fd4fcd3fd80bda5c063c5dfc3676b5df9b5fc0a0
andreia2007 <142910761+andreia2007@users.noreply.github.com>
2024-11-05 15:49:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz oi em inglês?',
+    'Oi em inglễs é HI (RAI)'
+)
+
